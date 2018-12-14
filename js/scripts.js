b55fdd6864d7b34ae8868bef3507a3cdaa09b374
Sarah Tuitoek <sjtoek@gmail.com>
2018-12-14 15:22:15 +0300

    Styled the user interface

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("button.easy").click(function (event){
+    $("#you").show();
+    $("#computer").show();
+    $(".panel").show();
+
+    $(".jumbotron").hide();
+  });
+  $("button.hard").click(function(){
+
+  });
+});
